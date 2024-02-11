fb2a3ec337ea81de7e9d8877bbf6e8bb20f1afef
Echenje <stephen.osiru@spinmobile.co.ke>
2024-02-11 16:34:09 +0300

    leetcode

--- api/scripts/leetcode.py
+++ api/scripts/leetcode.py
@@ -85,11 +85,6 @@
 
 class Solution(object):
 	def twoSum(self, nums, target):
-		"""
-		:type nums: List[int]
-		:type target: int
-		:rtype: List[int]
-		"""
 		return next((i, j) for i, x in enumerate(nums) for j, y in enumerate(nums[ i +1:], i+ 1) if x + y == target)
 
 #
